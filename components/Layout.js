e4fc2e01e2c18a1f97264eca5e03996316112390
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-03-14 15:55:44 -0700

    Updated asset and pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,31 @@
 import Head from 'next/head'
 import NavBar from './NavBar'
 import FooterLink from './FooterLink'
-import Reminder from './Reminder'
 
 const Layout = (props) => {
     return (
         <div>
             <Head>
-                <title>Gage - A Citizen Engagement Tool for Africa</title>
+                <title>Welcome to Ajax Classroom</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="" />
+                <meta name="author" content="" />
+                
                 <link href="/static/css/bootstrap.min.css" rel="stylesheet"/>
                 <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet"/>
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" type="text/javascript"></script>
-                <script src="/static/js/bootstrap.min.js" type="text/javascript"></script>
-                <link href="/static/css/style.css" rel="stylesheet"/>
+                <link href="/static/css/business-frontpage.css" rel="stylesheet"/>
                 <link href="/static/css/utilities.css" rel="stylesheet"/>
+
+                <script src="/static/js/bootstrap.min.js" type="text/javascript"></script>
+                <script src="/static/js/jquery.min.js" type="text/javascript"></script>                
             </Head>
             <div>
                 <NavBar />
                 <div>
                     {props.children}
                 </div>
-                <Reminder />
                 <FooterLink />
             </div> 
         </div>
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,59 +1,33 @@
 import Link from 'next/link'
-import '../static/css/style.css'
+import '../static/css/business-frontpage.css'
 
 const NavBar = () => {
     return (
         <div>
-        <nav id="nav" className="navbar navbar-expand-lg fixed-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/"><img className="logo" src="/static/images/gage-logo.png" /></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="fa fa-bars"></i>
-                </button>
-      
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item dropdown">
-                            <a className="r-mr-nav nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <div className="d-block d-md-none mobile-nav">
-                                    <h6><a href="/company">Company</a></h6>
-                                    <h6><a href="/career">Career</a></h6>
-                                </div>
-                                <div className="b-dropdown-menu d-none d-md-block">
-                                    <h6><a href="/company">Company</a></h6>
-                                    <h6><a href="/career">Career</a></h6>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="r-mr-nav nav-link">Learn </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="r-mr-nav nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Resources</a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <div className="d-block d-md-none mobile-nav">
-                                    <h6><a href="#">Rem Supply Chain</a></h6>
-                                </div>
-                                <div className="b-dropdown-menu d-none d-md-block">
-                                <h6><a href="#">Rem Supply Chain</a></h6>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a href="https://medium.com/@remhealth" className="r-mr-nav nav-link" target="_target">Blog</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/contact-us" className="r-mr-nav nav-link">Say Hi</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/signup"><button className="btn sign-up">Sign Up</button></a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/signin"><button className="btn signin">Login</button></a>
-                        </li>
-                    </ul>
-                </div>
+        <nav classNameName="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home
+                    <span className="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                </li>
+                </ul>
+            </div>
             </div>
         </nav>
     </div>
